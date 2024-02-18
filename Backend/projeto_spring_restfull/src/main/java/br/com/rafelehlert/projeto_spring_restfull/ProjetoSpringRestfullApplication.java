2a07ee24163e07979d9dee8c65eea04f565d6f25
Rafael Ehlert <rafaelehlert@MacBook-Pro-de-Rafael.local>
2024-02-18 09:55:15 -0300

    inicio do projeto

---
+++ Backend/projeto_spring_restfull/src/main/java/br/com/rafelehlert/projeto_spring_restfull/ProjetoSpringRestfullApplication.java
@@ -0,0 +1,13 @@
+package br.com.rafelehlert.projeto_spring_restfull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringRestfullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringRestfullApplication.class, args);
+	}
+
+}
---
+++ Backend/projeto_spring_restfull/src/test/java/br/com/rafelehlert/projeto_spring_restfull/ProjetoSpringRestfullApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rafelehlert.projeto_spring_restfull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringRestfullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
