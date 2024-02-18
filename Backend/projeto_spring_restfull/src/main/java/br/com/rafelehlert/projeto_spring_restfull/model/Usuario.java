dfa2938039cae9d606da94ab183287b308248357
Rafael Ehlert <rafaelehlert@MacBook-Pro-de-Rafael.local>
2024-02-18 11:15:30 -0300

    feat: Criada entidade de modelo para o abnco de dados

---
+++ Backend/projeto_spring_restfull/src/main/java/br/com/rafelehlert/projeto_spring_restfull/model/Usuario.java
@@ -0,0 +1,79 @@
+package br.com.rafelehlert.projeto_spring_restfull.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_usuario")
+public class Usuario {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String senha;
+    private String nome;
+
+    public Usuario(Long id, String login, String senha, String nome) {
+        this.id = id;
+        this.login = login;
+        this.senha = senha;
+        this.nome = nome;
+    }
+
+    public Usuario() {
+    }
+    
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getLogin() {
+        return login;
+    }
+    public void setLogin(String login) {
+        this.login = login;
+    }
+    public String getSenha() {
+        return senha;
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Usuario other = (Usuario) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+}
