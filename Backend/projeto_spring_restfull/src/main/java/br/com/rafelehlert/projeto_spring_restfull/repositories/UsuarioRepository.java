c216f34d59ab3a60fa3cd257eaa1a9de129585f8
Rafael Ehlert <ra.faehlert@hotmail.com>
2024-02-19 19:13:52 -0300

    feat: Criada consultas customizadas para login

--- Backend/projeto_spring_restfull/src/main/java/br/com/rafelehlert/projeto_spring_restfull/repositories/UsuarioRepository.java
+++ Backend/projeto_spring_restfull/src/main/java/br/com/rafelehlert/projeto_spring_restfull/repositories/UsuarioRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 import br.com.rafelehlert.projeto_spring_restfull.model.Usuario;
@@ -16,4 +17,9 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 
     @Query("SELECT obj FROM Usuario obj WHERE UPPER(obj.nome) LIKE UPPER(CONCAT('%', :nome, '%'))")
     Page<Usuario> procurarPorNome(String nome, Pageable pageable);
+
+    @Query("SELECT u FROM Usuario u WHERE u.login = ?1")
+    Usuario procuraUserPorLogin(String login);
+
+    UserDetails findByLogin(String login);
 }
